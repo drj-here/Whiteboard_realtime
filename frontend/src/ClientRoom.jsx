1ddbb9ca99835691a640059937cf695c46c10260
Dishu <dishuraj129@gmail.com>
2024-05-08 14:34:11 +0530

    Added client room page

--- frontend/src/ClientRoom.jsx
+++ frontend/src/ClientRoom.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { toast } from 'react-toastify'
 
-export default function ClientRoom() {
+export default function ClientRoom({userNum,socket,setUsers,setUserNum}) {
+    const imgRef=useRef(null)
+    useEffect(()=>{
+        socket.on("message",(data)=>{
+            toast.info(data.message)
+        })
+    },[])
+    useEffect(()=>{
+        socket.on("users",(data)=>{
+            setUsers(data)
+            setUserNum(data.length)
+        })
+    },[])
+    useEffect(()=>{
+        socket.on("canvasImage",(data)=>{
+            imgRef.current.src=data;
+        })
+    },[])
   return (
-    <div>
-      
+    <div className='container-fluid'>
+      <div className='row pb-2'>
+        <h1 className='display-5 pt-4 pb-3 text-center'>
+            React Drawing App- Users online:{userNum}
+        </h1>
+      </div>
+      <div className='row mt-5'>
+        <div className='col-md-8 overflow-hidden border border-dark px-0 mx-auto mt-3' style={{height:'500px'}}>
+            <img className='w-100 h-100' ref={imgRef} src='' alt='image'/>
+        </div>
+      </div>
     </div>
   )
 }
